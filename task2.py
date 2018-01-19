fa30e34853e8514b8f68923d6b7f0cc3d863c79b
KlwntSingh <kulwantbughipura@gmail.com>
2018-01-19 08:59:49 -0400

    reduce function implemented in task2

--- task2.py
+++ task2.py
@@ -18,3 +18,34 @@ def gcd(a, b, c):
     return gcd
 
 print(gcd(2, 3, 4))
+
+
+def gcd_reduce_fn(a, b):
+
+    div, num1, num2 = b
+    if num1 % div == 0 and num2 % div == 0:
+        return div
+    else:
+        return a
+
+from functools import reduce
+
+def gcd_with_reduce(a, b, c):
+    if a < b and a < c:
+        minNumber = a
+        otherNum1 = b
+        otherNum2 = c
+    elif b < a and b < c:
+        minNumber = b
+        otherNum1 = a
+        otherNum2 = c
+    else:
+        minNumber = c
+        otherNum1 = b
+        otherNum2 = a
+
+
+    ls=list(range(1, minNumber))
+    return reduce(gcd_reduce_fn, ls)
+
+print(gcd_with_reduce(2, 3, 4))
